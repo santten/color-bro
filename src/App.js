2a0555b47563c130ae794833a55abdb47f1cff5a
santten <santtusaaranen@gmail.com>
2024-09-28 18:00:07 +0300

    uses url params instead of sessionstorage

--- src/App.js
+++ src/App.js
@@ -1,36 +1,65 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 import ColorGenerator from './components/ColorGenerator';
-import Preview from './components/Preview'
 
 function App() {
-  const [yourDark, setYourDark] = useState(sessionStorage.getItem("yourDark"))
-  const [yourPrimary, setYourPrimary] = useState(sessionStorage.getItem("yourPrimary"))
-  const [yourAccent, setYourAccent] = useState(sessionStorage.getItem("yourAccent"))
-  const [yourSecondary, setYourSecondary] = useState(sessionStorage.getItem("yourSecondary"))
-  const [yourLight, setYourLight] = useState(sessionStorage.getItem("yourLight"))
+
+  const getQueryParam = (param, defaultValue) => {
+    const urlParams = new URLSearchParams(window.location.search);
+    return urlParams.get(param) || defaultValue;
+  };
+
+  const defaulttheme = {
+    dark: "#000000",
+    primary: "#a0295b",
+    accent: "#232323",
+    secondary: "#231456",
+    light: "#ffffff",
+  }
+
+
+  const [yourDark, setYourDark] = useState(getQueryParam("yourDark", defaulttheme.dark))
+  const [yourPrimary, setYourPrimary] = useState(getQueryParam("yourPrimary", defaulttheme.primary))
+  const [yourAccent, setYourAccent] = useState(getQueryParam("yourAccent", defaulttheme.accent))
+  const [yourSecondary, setYourSecondary] = useState(getQueryParam("yourSecondary", defaulttheme.secondary))
+  const [yourLight, setYourLight] = useState(getQueryParam("yourLight", defaulttheme.light))
 
   const [previewColors, setPreviewColors] = useState(false)
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("yourDark", yourDark);
+    urlParams.set("yourPrimary", yourPrimary);
+    urlParams.set("yourAccent", yourAccent);
+    urlParams.set("yourSecondary", yourSecondary);
+    urlParams.set("yourLight", yourLight);
+    window.history.replaceState(null, null, "?" + urlParams.toString());
+  }, [yourDark, yourPrimary, yourAccent, yourSecondary, yourLight]);
+
+  useEffect(() => {
+    document.documentElement.style.setProperty('--background-color', previewColors ? yourLight : defaulttheme.light);
+    document.documentElement.style.setProperty('--text-color', previewColors ? yourDark : defaulttheme.dark);
+    document.documentElement.style.setProperty('--primary-color', previewColors ? yourPrimary : defaulttheme.primary);
+    document.documentElement.style.setProperty('--accent-color', previewColors ? yourAccent : defaulttheme.accent);
+    document.documentElement.style.setProperty('--secondary-color', yourSecondary ? yourSecondary : defaulttheme.secondary);
+  }, [yourDark, yourPrimary, yourAccent, yourSecondary, yourLight, previewColors]);
 
   const togglePreviewColors = (e) => {
     setPreviewColors(!previewColors)
   }
 
-  const defaulttheme = {
-    light: "#ffffff",
-    dark: "#000000",
-    primary: "#a0295b"
-  }
+  useEffect(() => {
+    document.body.style.backgroundColor = previewColors ? yourDark : defaulttheme.dark
+  }, [yourDark]);
 
   return (
     <div>
+      <div className="mainDiv" style={{ backgroundColor: previewColors ? yourLight : defaulttheme.light }}>
+        <h3 style={{ color: previewColors ? yourPrimary : defaulttheme.primary }}>
+          ColorBro
+        </h3>
 
-      <h3 style={{color: previewColors ? yourPrimary : defaulttheme.primary}}>
-      ColorBro
-      </h3>
-
-      <div className="mainDiv" style={{backgroundColor: previewColors ? yourLight : defaulttheme.light}}>
         <ColorGenerator
           yourDark={yourDark} setYourDark={setYourDark}
           yourPrimary={yourPrimary} setYourPrimary={setYourPrimary}
@@ -38,11 +67,8 @@ function App() {
           yourSecondary={yourSecondary} setYourSecondary={setYourSecondary}
           yourLight={yourLight} setYourLight={setYourLight}
         />
-        <button onClick={togglePreviewColors} style={{
-          backgroundColor: previewColors ? yourDark : defaulttheme.dark,
-          color: previewColors ? yourLight : defaulttheme.light
-        }}>
-          {previewColors ? "Make Website Use Palette Colors" : "Back To Default"}
+        <button onClick={togglePreviewColors}>
+          {previewColors ? "Use Neutral Colors in UI" : "Use Palette Colors in UI"}
         </button>
       </div>
 
